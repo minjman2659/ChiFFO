4ee16d814eefde5caabddae3f20a17852ec83cd2
edan_3000 <roseswallow13@gmail.com>
2021-11-11 19:22:41 +0900

    [김태일] problems solved

---
+++ 김태일/211111/비밀지도.js
@@ -0,0 +1,18 @@
+const solution = (n, arr1, arr2) => {
+  let answer = [];
+
+  for (let i = 0; i < n; i++) {
+    const bin = (arr1[i] | arr2[i]).toString(2); // 두 피연산자의 각 자리 비트의 값이 하나라도 1이면 1을 반환합니다.
+    let line = [];
+
+    for (let j = bin.length - n; j < bin.length; j++) {
+      if (bin[j] === '1') {
+        line.push('#');
+      } else {
+        line.push(' ');
+      }
+    }
+    answer.push(line.join(''));
+  }
+  return answer;
+}

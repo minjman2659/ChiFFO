8ec5cc30a105020d495806b6e79d5e6a01f1fbcc
edan_3000 <roseswallow13@gmail.com>
2021-12-01 19:41:48 +0900

    [김태일] problems solved

---
+++ 김태일/211201/거스름돈.js
@@ -0,0 +1,12 @@
+const solution = (n, money) => {
+    let answer = 0;
+    let dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+    money.forEach((el, idx) => {
+        for (let i = el; i < n + 1; i++) {
+            dp[i] += dp[i - el];
+        }
+    });
+    answer = dp[n] % 1000000007;
+    return answer;
+}
